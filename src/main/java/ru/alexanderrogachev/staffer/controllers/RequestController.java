d608e74d440143de3a547b2982b318c1ee52df6a
Alexander Rogachev <x-stelz@mail.ru>
2022-11-11 00:45:18 +0400

    added base functionality of adding staffers to request, editing requests

--- src/main/java/ru/alexanderrogachev/staffer/controllers/RequestController.java
+++ src/main/java/ru/alexanderrogachev/staffer/controllers/RequestController.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.alexanderrogachev.staffer.domain.User;
 import ru.alexanderrogachev.staffer.models.Request;
+import ru.alexanderrogachev.staffer.models.Staffer;
 import ru.alexanderrogachev.staffer.services.RequestServiceImpl;
+import ru.alexanderrogachev.staffer.services.StafferServiceImpl;
+import ru.alexanderrogachev.staffer.services.UserDetailsServiceImpl;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -14,18 +20,24 @@ public class RequestController {
 
     private final RequestServiceImpl requestService;
 
+    private final StafferServiceImpl stafferService;
+
+    private final UserDetailsServiceImpl userDetailsService;
+
     @Autowired
-    public RequestController(RequestServiceImpl requestService) {
+    public RequestController(RequestServiceImpl requestService, StafferServiceImpl stafferService, UserDetailsServiceImpl userDetailsService) {
         this.requestService = requestService;
+        this.stafferService = stafferService;
+        this.userDetailsService = userDetailsService;
     }
 
     //________________________________________
-    //Страница добавления и отображения заявок
+    //Страница отображения заявок
     //________________________________________
 
     //Отображение списка запросов и фильтрация
-    @GetMapping("/requests/add_request")
-    public String requests(@RequestParam(required = false) String filter, Model model) {
+    @GetMapping("/requests")
+    public String requestsPage(@RequestParam(required = false) String filter, Model model) {
         List<Request> requests = requestService.getAllRequests();
         List<Request> filterRequests;
         if (filter != null && !filter.isEmpty()) {
@@ -34,6 +46,31 @@ public class RequestController {
             filterRequests = requests;
         }
         model.addAttribute("filterRequests", filterRequests);
+        return "requests";
+    }
+
+    //Добавление сотрудника в заявку по id
+    @PostMapping("/requests/{requestId}")
+    public String addStafferToRequest(HttpServletRequest http, @PathVariable("requestId") int requestId) {
+        Principal principal = http.getUserPrincipal();
+        User user = userDetailsService.findUserByUsername(principal.getName()).get();
+        Staffer staffer = user.getStaffer();
+        Request request = requestService.getRequest(requestId);
+        List<Staffer> staffers = request.getRequestStaffers();
+        staffers.add(staffer);
+        request.setRequestStaffers(staffers);
+        requestService.saveRequest(request);
+        stafferService.saveStaffer(staffer);
+        return "redirect:/requests";
+    }
+
+    //________________________________________
+    //Страница добавления заявок
+    //________________________________________
+
+    //Отображение страницы добавления заявки
+    @GetMapping("/requests/add_request")
+    public String addRequestPage() {
         return "requests/add_request";
     }
 
@@ -64,7 +101,7 @@ public class RequestController {
         request.setDateOfRequest(oldVersionOfRequest.getDateOfRequest());
         request.setRequestId(requestId);
         requestService.saveRequest(request);
-        return "redirect:/requests/add_request";
+        return "redirect:/requests";
     }
 
     //_______________________________________________
@@ -73,8 +110,10 @@ public class RequestController {
 
     //Отображение страницы со списком сотрудников
     @GetMapping("/requests/staffers_list/{requestId}")
-    public String staffersListPage(@ModelAttribute("request") Request request, @PathVariable("requestId") int requestId){
-
+    public String staffersListPage(Model model, @PathVariable("requestId") int requestId) {
+        Request request = requestService.getRequest(requestId);
+        List<Staffer> staffers = request.getRequestStaffers();
+        model.addAttribute("staffers", staffers);
         return "requests/staffers_list";
     }
 
--- src/main/java/ru/alexanderrogachev/staffer/repositories/RequestRepository.java
+++ src/main/java/ru/alexanderrogachev/staffer/repositories/RequestRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface RequestRepository extends JpaRepository<Request, Integer> {
     List<Request> findByShopName(String shopName);
+
 }
